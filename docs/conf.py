82222cd99fd0cc0b9b2b78e52986b04069d15d2a
Isaac.Lera <isaac.lera@gmail.com>
2022-09-29 11:08:14 +0200

    toc update

--- docs/conf.py
+++ docs/conf.py
@@ -57,3 +57,5 @@ highlight_language = 'python3'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+nbsphinx_allow_errors = True
